0e6e57a2bba78d2142bc9a2c68a042b0ef64c115
NumanArshad <37812712+NumanArshad@users.noreply.github.com>
2018-04-25 19:47:39 +0500

    i created pharmacy server with functionality to delete product and show all product

---
+++ PharmacyServer/PharmacyServer/StockDL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PharmacyServer
+{
+    public class StockDL
+    {
+        public static List<StockClass> stocklist = new List<StockClass>();
+        public static void addProduct(StockClass product)
+        {
+            stocklist.Add(product);
+        }
+        public void deleteProduct(int ind)
+        {
+
+            StockDL.stocklist.RemoveAt(ind);
+
+
+        }
+    }
+}
