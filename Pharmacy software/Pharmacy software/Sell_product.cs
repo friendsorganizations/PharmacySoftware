954224f5b562a06b3198d2ca52dffbc1c620dc57
NumanArshad <37812712+NumanArshad@users.noreply.github.com>
2018-04-25 23:13:03 +0500

    I call server functionality of search product by name,type and formula

---
+++ Pharmacy software/Pharmacy software/Sell_product.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pharmacy_software
+{
+    public partial class Sell_product : Form
+    {
+        public Sell_product()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdCellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdAddOrder(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdDeleteOrder(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdPrintOrder(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox12_TextChanged_1(object sender, EventArgs e)
+        {
+
+            localhost.Service1 s = new localhost.Service1();
+            BindingSource k = new BindingSource();
+            k.DataSource = s.searchName(textBox12.Text);
+            dataGridView1.DataSource = k;
+            dataGridView1.Columns[1].Visible = false;
+            dataGridView1.Columns[5].Visible = false;
+            dataGridView1.Columns[7].Visible = false;
+        }
+        DataTable t = new DataTable();
+        private void Sell_product_Load(object sender, EventArgs e)
+        {
+            t.Columns.Add("ExpiryDate", typeof(string));
+            t.Columns.Add("Expiryspec", typeof(string));
+            t.Columns.Add("ItemFormula", typeof(string));
+            t.Columns.Add("ItemName", typeof(string));
+            t.Columns.Add("ItemPrice", typeof(int));
+            t.Columns.Add("ItemPricespec", typeof(int));
+            t.Columns.Add("ItemQuantity", typeof(int));
+            t.Columns.Add("Itemquantityspec", typeof(int));
+            t.Columns.Add("ItemType", typeof(string));
+
+            dataGridView1.DataSource = t;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+            localhost.Service1 s = new localhost.Service1();
+            BindingSource k = new BindingSource();
+            k.DataSource = s.searchFormula(textBox3.Text);
+            dataGridView1.DataSource = k;
+            dataGridView1.Columns[1].Visible = false;
+            dataGridView1.Columns[5].Visible = false;
+            dataGridView1.Columns[7].Visible = false;
+        }
+
+        private void mainToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 m = new Form1();
+            this.Hide();
+            m.Show();
+        }
+    }
+}
